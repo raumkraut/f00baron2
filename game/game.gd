ea26d5230c33e8db19ae186111dcaa7b8199a5f7
Mel Collins <mel@raumkraut.net>
2016-07-15 17:22:53 +0200

    Mr Hammond, the clouds are working

---
+++ game/game.gd
@@ -0,0 +1,8 @@
+
+extends Node2D
+
+func _init():
+	randomize()
+
+func _ready():
+	pass
---
+++ game/scenery/cloud.gd
@@ -0,0 +1,43 @@
+
+extends Sprite
+
+# The rect in which the cloud can respawn
+var airspace
+# Control cloud motion
+var max_speed = 200
+var min_speed = 20
+var velocity
+
+func _ready():
+	# Restrict the cloud to the parent container's rect (eg. a VisibilityNotifier2D)
+	airspace = get_node('..').get_rect()
+	respawn()
+	set_process(true)
+	
+
+func _process(delta):
+	# Move the cloud a bit
+	var new_pos = get_pos() + (velocity * delta)
+	set_pos(new_pos)
+	# Respawn if we've gorn orf
+	if not airspace.has_point(new_pos):
+		respawn()
+
+func respawn():
+	"""
+		Prep the cloud to (re-)enter the scene
+	"""
+	var altitude = rand_range(airspace.pos.y, airspace.end.y)
+	# Whether to enter stage left (1) or right (-1)
+	var direction = sign(randf() - 0.5)
+	var new_pos
+	if direction > 0:
+		new_pos = Vector2(airspace.pos.x, altitude)
+	else:
+		new_pos = Vector2(airspace.end.x, altitude)
+	set_pos(new_pos)
+	
+	# Motion
+	var speed = rand_range(min_speed, max_speed)
+	velocity = Vector2(direction * speed, 0)
+	
