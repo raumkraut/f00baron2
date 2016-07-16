b611d8de27b4e0496fb111147192921071ff0786
Mel Collins <mel@raumkraut.net>
2016-07-17 01:14:37 +0200

    Add basic flight model and player 1 controls

--- game/game.gd
+++ game/game.gd
@@ -8,4 +8,25 @@ func _ready():
 	# Set the player colours
 	get_node("airspace/player1").set_colour('ff0000')
 	get_node("airspace/player2").set_colour('aa88ff')
+	set_process(true)
+	
+
+func _process(delta):
+	# Tell the planes what to do
+	var player1 = get_node("airspace/player1")
+	if Input.is_action_pressed("p1_throttle_up"):
+		player1.set_throttle(1)
+	elif Input.is_action_pressed("p1_throttle_down"):
+		player1.set_throttle(0)
+	if Input.is_action_pressed("p1_clockwise"):
+		player1.set_pitching(-1)
+	elif Input.is_action_pressed("p1_anticlockwise"):
+		player1.set_pitching(1)
+	else:
+		player1.set_pitching(0)
+	player1.set_firing(Input.is_action_pressed("p1_fire"))
+	# Player 2
+	## TODO
+	# Check for planes going out of bounds
+	## TODO
 	
--- game/plane/plane.gd
+++ game/plane/plane.gd
@@ -3,13 +3,35 @@ extends Node2D
 
 # The opacity of the propellor when it's not fully visible
 var prop_opacity = 0.7
-
+# Flight controls
+var throttle = 0
+var pitching = 0
+var firing = false
+# Flight parameters
+var drag = 0.98
+var vertical_drag = 0.4
+var lift = 1000
+var turn_rate = 3
+var thrust = 300
+var velocity = Vector2()
+var stall_speed = 100
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_process(true)
 
+func _process(delta):
+	# Move
+	set_pos(get_pos() + (velocity * delta))
+	# Rotate
+	set_rot(get_rot() + (pitching * turn_rate * delta))
+	# Accelerations and decelerations
+	var facing = Vector2(1, 0).rotated(get_rot())
+	var speed = velocity.dot(facing) * drag
+	var belly = Vector2(0, 1).rotated(get_rot())
+	var belly_speed = velocity.dot(belly) * vertical_drag
+	velocity = Vector2(speed + (throttle * thrust * delta), belly_speed).rotated(get_rot())
+	
+	## TODO: Shoot
 
 func _on_rotator_timeout():
 	""" Spin the propellor """
@@ -19,10 +41,26 @@ func _on_rotator_timeout():
 	else:
 		prop.set_opacity(prop_opacity)
 
+
 func set_colour(colour):
 	""" Set the colour of the fuselage and wings """
 	var fuselage = get_node('fuselage')
 	var wings = get_node('wings')
 	fuselage.set_modulate(colour)
 	wings.set_modulate(colour)
+	
+func set_throttle(brum):
+	""" Full throttle or no throttle, nothing else """
+	throttle = float(brum)
+	
+func set_pitching(neeyow):
+	""" You're either with the clock or against the clock """
+	if neeyow == 0:
+		pitching = 0
+	else:
+		pitching = sign(neeyow)
+	
+func set_firing(dakka):
+	""" Dakka dakka dakka! """
+	firing = bool(dakka)
 	
